0b536489404e16016898f5fc690d78895a7db331
kapillohana <kapillohana79@gmail.com>
2024-04-21 23:32:48 +0500

    CLI Based Currency Converter

---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env ts-node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, //BASE CURRENCY
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: "Enter Your Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'to',
+        message: "Select the currency in which you want to convert",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'Amount',
+        message: 'Enter the amount you want to convert',
+        type: 'number',
+    }
+]);
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let Amount = userAnswer.Amount;
+let baseAmount = Amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount.toFixed(2));
